3ef018a9e3483240648e8e3d93257cfbf5eb7f3b
lirenhao <renhao.li@bjyada.com>
2017-09-29 08:23:09 +0800

    模板修改

--- src/main/java/com/yada/dao/OrganizationDao.java
+++ src/main/java/com/yada/dao/OrganizationDao.java
@@ -2,11 +2,11 @@ package com.yada.dao;
 
 import com.yada.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * Organization 表数据库控制层接口
  */
-
-public interface OrganizationDao extends JpaRepository<Organization, Long> {
+public interface OrganizationDao extends JpaRepository<Organization, Long>, JpaSpecificationExecutor<Organization> {
 
 }
